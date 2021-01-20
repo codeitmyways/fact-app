4a27f9a4b6c2afd9cbfd62be9ffe4497bb3cbd41
Sumit meena <in.sumitmeena@gmail.com>
2021-01-20 09:49:42 +0530

    1. Setup Project

---
+++ Fact-App/Fact-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fact-App
+//
+//  Created by Sumit meena on 20/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
