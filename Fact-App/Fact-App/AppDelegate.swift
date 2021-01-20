5011548f65f8f7f3f9a7da0172571d671e4a9c35
Sumit meena <in.sumitmeena@gmail.com>
2021-01-20 10:08:28 +0530

    1. remove storyboard from project

--- Fact-App/Fact-App/AppDelegate.swift
+++ Fact-App/Fact-App/AppDelegate.swift
@@ -10,11 +10,19 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        if let window = window{
+            let homeViewController = UIViewController()
+            homeViewController.view.backgroundColor = UIColor.red
+            window.rootViewController = homeViewController
+        }
+        
+
         return true
     }
 
